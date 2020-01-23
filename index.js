c59742f64f4b98d6e58f9ed37318086ed9ad71da
lowryjs <47071190+lowryjs@users.noreply.github.com>
2020-01-23 23:37:38 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blake</title>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js"></script>
+
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+	<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	<link href="style.css" rel="stylesheet">
+
+	<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+	<script type="text/javascript" src="index.js"></script>
+
+	<style type="text/css">
+
+	</style>
+</head>
+<body>
+	<div id="background"></div>
+	<div class="container-fluid">
+		<div class="row box">
+			<div class="col-8 offset-2">
+				<div id="border" data-aos="zoom-in">
+					<div id="title">
+						👋🏻 Hello, I'm <b>Blake Lowry</b>!
+					</div>
+					<div>
+						<div id="main" class="content">
+							I am a front-end website developer based in Ontario focused on delivering fast, responsive and secure websites. 
+						</div>
+						<div id="second" class="content">
+							👷 This website is currently under construction!<br>
+							👉 Get into contact with me <a id="email" href="mailto:1boydlowrybl@hdsb.ca">here</a>!
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script type="text/javascript">
+		AOS.init()
+	</script>
+</body>
+</html>
